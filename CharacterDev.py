ae85a5b03810ca1862687bd512129f083095335d
B.Loretta Lewis <you@example.com>
2022-08-21 18:03:39 -0500

    UPDATED with Enemy One complete

--- CharacterDev.py
+++ CharacterDev.py
@@ -1,12 +1,12 @@
 print ("Welcome to the Industry Wars")
 print("Prepare for the survival of the Fittest!!")
 
-my_hero {
-    "name" : "Giselle_Fire_Song"
-    "level" : 1,
+my_hero = {
+    "name" : "Giselle_Fire_Song",
+    "level": 1,
     "health" : 100,
     "equipment" : {'Stilettos'},
-    "attacks" : (("power vocals", 100), ("killer beehive", 85), ("explosive dance moves", 70)),
+    "attacks" : (("power vocals", 100), ("killer beehive", 85), ("explosive dance moves", 70), ("grammy throws", 50)),
     "coins" : {
         "gold": 0,
         "platinum": 0,
@@ -15,5 +15,39 @@ my_hero {
     "phrases": {
         "challenges": ['WHO RUNS THE WORLD!!', 'IM FLAWLESS!!', 'IM THAT GIRL!!'],
         "pleas": ['You wont break my soul..just my leg', 'Pretty hurts, and so did that', 'ohh..that made me lose my breath!']
+    }    
+}
+
+enemy_one = {
+    "name" : "BarbZ",
+    "level" : 2,
+    "health": 100,
+    "equipment" : {'Cape'},
+    "attacks" : (("Roman summons", 100), ('spin kick'),60 ('anaconda attack', 50)),
+    "coins" : {
+        "gold": 35,
+        "platinum": 12,
+        "diamond": 1,
+    "phrases": {
+        "challenges": ['Rah, Rah like a dungeon dragon!!', 'Never sleep on me!', 'You better watch out for me!'],
+        "plea": ['No not that!', ' oof, youre killing me', 'Anything but the Beehive!!'],
+    }
+    }
+
+}
+
+enemy_two = {
+    "name" : "Naval Colonel",
+    "level" : 3,
+    "health": 100,
+    "equipment" : {'microphone'},
+    "attacks" : (("", 100), (''),60 ('', 50)),
+    "coins" : {
+        "gold": 35,
+        "platinum": 12,
+        "diamond": 1,
+    "phrases": {
+        "challenges": ['Rah, Rah like a dungeon dragon!!', 'Never sleep on me!', 'You better watch out for me!'],
+        "plea": ['No not that!', ' oof, youre killing me', 'Anything but the Beehive!!'],
     }
 }
