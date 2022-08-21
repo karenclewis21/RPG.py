c1cb9bc30c6adacd0e384ac7c4b69239ae60e8ce
B.Loretta Lewis <you@example.com>
2022-08-21 15:53:43 -0500

    File name change and Hero Development

--- CharacterDev.py
+++ CharacterDev.py
@@ -1,1 +1,19 @@
-print ("Welcome to Pergatory")
+print ("Welcome to the Industry Wars")
+print("Prepare for the survival of the Fittest!!")
+
+my_hero {
+    "name" : "Giselle_Fire_Song"
+    "level" : 1,
+    "health" : 100,
+    "equipment" : {'Stilettos'},
+    "attacks" : (("power vocals", 100), ("killer beehive", 85), ("explosive dance moves", 70)),
+    "coins" : {
+        "gold": 0,
+        "platinum": 0,
+        "diamond": 0, 
+    },
+    "phrases": {
+        "challenges": ['WHO RUNS THE WORLD!!', 'IM FLAWLESS!!', 'IM THAT GIRL!!'],
+        "pleas": ['You wont break my soul..just my leg', 'Pretty hurts, and so did that', 'ohh..that made me lose my breath!']
+    }
+}
